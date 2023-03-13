381020651b0f3a07bdf5d128b7a79ee9f7b27834
Adriano <adriano_alves_2008@hotmail.com>
2023-03-13 14:15:38 -0300

    adicionando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import { ThemeProvider } from 'styled-components'
 import Header from './components/Header'
 import { Global } from './styles/GlobalStyle'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { useContext } from 'react'
 import { ThemeContext } from './contexts/theme'
+
 import Main from './components/Main'
-import ComoComprar from './components/como-comprar'
+import Carrinho from './pages/Carrinho'
 
 function App() {
   const { theme } = useContext(ThemeContext)
   return (
     <ThemeProvider theme={theme}>
-      <Global />
-      <Header />
-      <Main />
+      <BrowserRouter>
+        <Global />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/carrinho" element={<Carrinho />} />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
