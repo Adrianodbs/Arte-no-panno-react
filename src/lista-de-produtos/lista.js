0504e41330d55ac2ed622ecc1c0b66c158d693e0
Adriano <adriano_alves_2008@hotmail.com>
2023-06-29 14:03:49 -0300

    remoção do caminho public

--- src/lista-de-produtos/lista.js
+++ src/lista-de-produtos/lista.js
@@ -3,133 +3,133 @@ export const Lista = [
     id: 1,
     nome: 'Chaveiro com bordado de nuvem',
     valor: 15,
-    imagem: '/public/assets/c1.jpeg',
+    imagem: '/assets/c1.jpeg',
     tipo: 'chaveiro'
   },
   {
     id: 2,
     nome: 'Chaveiro com bordado de coração',
     valor: 15,
-    imagem: '/public/assets/c2.jpeg',
+    imagem: '/assets/c2.jpeg',
     tipo: 'chaveiro'
   },
   {
     id: 3,
     nome: 'Chaveiro com bordado de morango',
     valor: 15,
-    imagem: '/public/assets/c3.jpeg',
+    imagem: '/assets/c3.jpeg',
     tipo: 'chaveiro'
   },
   {
     id: 4,
     nome: 'Chaveiro com bordado de morango colorido',
     valor: 15,
-    imagem: '/public/assets/cv3.png',
+    imagem: '/assets/cv3.png',
     tipo: 'chaveiro'
   },
   {
     id: 5,
     nome: 'Mural 1',
     valor: 120,
-    imagem: '/public/assets/m1.jpeg',
+    imagem: '/assets/m1.jpeg',
     tipo: 'mural'
   },
   {
     id: 6,
     nome: 'Mural 2',
     valor: 180,
-    imagem: '/public/assets/m2.jpeg',
+    imagem: '/assets/m2.jpeg',
     tipo: 'mural'
   },
   {
     id: 7,
     nome: 'Mural 3',
     valor: 180,
-    imagem: '/public/assets/m3.jpeg',
+    imagem: '/assets/m3.jpeg',
     tipo: 'mural'
   },
   {
     id: 8,
     nome: 'Mural 4',
     valor: 180,
-    imagem: '/public/assets/m4.jpeg',
+    imagem: '/assets/m4.jpeg',
     tipo: 'mural'
   },
   {
     id: 9,
     nome: 'Mural 5',
     valor: 150,
-    imagem: '/public/assets/m5.jpeg',
+    imagem: '/assets/m5.jpeg',
     tipo: 'mural'
   },
   {
     id: 10,
     nome: 'Mural 6',
     valor: 180,
-    imagem: '/public/assets/m6.jpeg',
+    imagem: '/assets/m6.jpeg',
     tipo: 'mural'
   },
   {
     id: 11,
     nome: 'Mural 7',
     valor: 160,
-    imagem: '/public/assets/m7.jpeg',
+    imagem: '/assets/m7.jpeg',
     tipo: 'mural'
   },
   {
     id: 12,
     nome: 'Bordado em T-shirt (gratidão)',
     valor: 40,
-    imagem: '/public/assets/p1.jpeg',
+    imagem: '/assets/p1.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 13,
     nome: 'Bordado em T-shirt (espalhe amor)',
     valor: 40,
-    imagem: '/public/assets/p2.jpeg',
+    imagem: '/assets/p2.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 14,
     nome: 'Bordado em T-shirt (girassol)',
     valor: 40,
-    imagem: '/public/assets/p3.jpeg',
+    imagem: '/assets/p3.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 15,
     nome: 'Bordado em T-shirt (religioso)',
     valor: 40,
-    imagem: '/public/assets/p4.jpeg',
+    imagem: '/assets/p4.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 16,
     nome: 'Bordado em T-shirt (casais)',
     valor: 40,
-    imagem: '/public/assets/p5.jpeg',
+    imagem: '/assets/p5.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 17,
     nome: 'Bordado em T-shirt (frases musicais)',
     valor: 40,
-    imagem: '/public/assets/p7.jpeg',
+    imagem: '/assets/p7.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 18,
     nome: 'Bordado em T-shirt (borboleta)',
     valor: 40,
-    imagem: '/public/assets/p8.jpeg',
+    imagem: '/assets/p8.jpeg',
     tipo: 't-shirt'
   },
   {
     id: 19,
     nome: 'Bordado em T-shirt (cavalo)',
     valor: 40,
-    imagem: '/public/assets/p9.jpeg',
+    imagem: '/assets/p9.jpeg',
     tipo: 't-shirt'
   }
 ]
